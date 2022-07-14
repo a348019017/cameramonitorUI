a7689d0e05f4779597f55dde07ab6a534ae843f8
zhouyuxiang <755754306@qqcom>
2022-07-14 18:25:48 +0800

    新增mapbox底图

---
+++ src/views/map/js/command/CesiumdrawCircleTool.js
@@ -0,0 +1,207 @@
+
+
+import * as turf from "@turf/turf"
+import { GUID } from "../util";
+
+//绘制圆形的包装
+export class CesiumDrawCircleTool {
+
+
+  constructor(_callback, option) {
+    this.drawcomplete = _callback;
+
+    //构造函数中作一些判断
+    if (!viewer.scene.pickPositionSupported) {
+      window.alert("This browser does not support pickPosition.");
+    }
+    //默认绘制线
+    this.drawingMode = "line";
+    if (option && option.drawingMode) {
+      this.drawingMode = option.drawingMode;
+    }
+    //默认导出为4326参考系的点
+    if (option && option.projection) {
+      this.projection = option.projection ? option.projection : "EPSG:4326";
+    }
+    this.clampToGround = false;
+    if (option && option.clampToGround) {
+      this.clampToGround = option.clampToGround;
+    }
+
+    this.activeShapePoints = [];
+    this.activeShape = undefined;
+    this.activePoints = [];
+    this.floatingPoint = undefined;
+
+    this.handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+    //右键结束的时候是否清除对象
+    this.isclearwhencomplete = true;
+    if (option && option.isclearwhencomplete === false) {
+      this.isclearwhencomplete = false;
+    }
+  }
+
+
+  projectto(position) {
+    var ellipsoid = viewer.scene.globe.ellipsoid;
+    var cartesian3 = position;
+    var cartographic = ellipsoid.cartesianToCartographic(cartesian3);
+    var lat = Cesium.Math.toDegrees(cartographic.latitude);
+    var lng = Cesium.Math.toDegrees(cartographic.longitude);
+    var alt = cartographic.height;
+    return [lng, lat, alt]
+  }
+
+
+
+
+  active() {
+
+    this.activeShapePoints = [];
+    this.activeShape = undefined;
+    this.activePoints = [];
+    this.floatingPoint = undefined;
+    //暂时不详
+    // viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
+    //     Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK
+    // );
+    //必须开启深度测试，这时候桥在地形下方
+    viewer.scene.globe.depthTestAgainstTerrain = true
+    let that = this;
+
+    this.handler.setInputAction(function (event) {
+      // We use `viewer.scene.pickPosition` here instead of `viewer.camera.pickEllipsoid` so that
+      // we get the correct point when mousing over terrain.
+      var earthPosition = viewer.scene.pickPosition(event.position);
+      // `earthPosition` will be undefined if our mouse is not over the globe.
+      if (Cesium.defined(earthPosition)) {
+        if (that.activeShapePoints.length === 0) {
+          that.floatingPoint = that.createPoint(earthPosition);
+          that.activeShapePoints.push(earthPosition);
+          var dynamicPositions = new Cesium.CallbackProperty(function () {
+            if (that.drawingMode === "polygon") {
+              return new Cesium.PolygonHierarchy(that.activeShapePoints);
+            }
+            return that.activeShapePoints;
+          }, false);
+          that.activeShape = that.drawShape(dynamicPositions);
+        }
+        that.activeShapePoints.push(earthPosition);
+        that.activePoints.push(that.createPoint(earthPosition));
+      }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    this.handler.setInputAction(function (event) {
+      if (Cesium.defined(that.floatingPoint)) {
+        var newPosition = viewer.scene.pickPosition(event.endPosition);
+        if (Cesium.defined(newPosition)) {
+          that.floatingPoint.position.setValue(newPosition);
+          that.activeShapePoints.pop();
+          that.activeShapePoints.push(newPosition);
+        }
+      }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+
+    this.handler.setInputAction(function (event) {
+      that.terminateShape();
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+
+
+
+
+  }
+
+  deactive() {
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    this.handler.removeInputAction(Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+    //清除掉相关资源
+    viewer.entities.remove(this.floatingPoint);
+    viewer.entities.remove(this.activeShape);
+    for (let index = 0; index < this.activePoints.length; index++) {
+      const element = this.activePoints[index];
+      viewer.entities.remove(element);
+    }
+    //
+
+  }
+
+  //右键结束
+  terminateShape() {
+    this.activeShapePoints.pop();
+    var obj = this.drawShape(this.activeShapePoints);
+    viewer.entities.remove(this.floatingPoint);
+    viewer.entities.remove(this.activeShape);
+    var that = this;
+    //有值的时候才返回
+    if (this.drawcomplete && this.activeShapePoints) {
+      try {
+        //多边形的点返回额外的起点
+        let returnpoints = this.activeShapePoints;
+        let geojson = undefined;
+        if (this.drawingMode == "polygon") {
+          returnpoints = [].concat(this.activeShapePoints);
+          returnpoints.push(this.activeShapePoints[0]);
+          //转换成经纬度然后构造geojson
+          let coords = returnpoints.map(p => { return that.projectto(p) });
+          //给予一个随机guid作为名称，用于检索
+          geojson = turf.polygon([coords], { name: GUID() });
+        }
+        //转换成
+        this.drawcomplete({
+          id: obj,
+          pos: returnpoints,
+          geojson: geojson
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (this.isclearwhencomplete) {
+      viewer.entities.remove(obj);
+      for (let index = 0; index < this.activePoints.length; index++) {
+        const element = this.activePoints[index];
+        viewer.entities.remove(element);
+      }
+    }
+    this.floatingPoint = undefined;
+    this.activeShape = undefined;
+    this.activeShapePoints = [];
+    this.activePoints = [];
+
+  }
+
+  drawShape(center, radians) {
+    var shape;
+    shape = viewer.entities.add({
+      position: center,
+      ellipse: {
+        hierarchy: positionData,
+        material: new Cesium.ColorMaterialProperty(
+          Cesium.Color.WHITE.withAlpha(0.7)
+        ),
+        // clampToGround: this.clampToGround,此后的api将没有clamptogroup参数，使用perposition决定是否贴地
+        perPositionHeight: !this.clampToGround,
+      },
+    });
+
+    return shape;
+  }
+
+  //创建点
+  createPoint(worldPosition) {
+    var point = viewer.entities.add({
+      position: worldPosition,
+      point: {
+        color: Cesium.Color.RED,
+        pixelSize: 5,
+        heightReference: Cesium.HeightReference.NONE,
+      },
+    });
+    return point;
+  }
+}
+
+
+
--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -1,77 +1,76 @@
 
 </style>
 <style lang="scss" scoped>
-
 .maproot {
-   .easyearthContainer {
+  .easyearthContainer {
     width: 100%;
-   height: calc(100vh - 214px);
+    height: calc(100vh - 214px);
   }
 }
-  
-
-
-
-
-
 </style>
 <template>
-    <div class="maproot">
-     <div id="easyearthContainer" :style="mapSelect"
-      :class="[enableMultiView ? 'easyearthMultiContainer' : 'easyearthContainer']"
-    ></div>
-     <toolbar v-if="showtoolbar" :project="curproject"></toolbar>
-    </div>
-    
+  <div class="maproot">
+    <div id="easyearthContainer" :style="mapSelect"
+      :class="[enableMultiView ? 'easyearthMultiContainer' : 'easyearthContainer']"></div>
+    <toolbar v-if="showtoolbar" :project="curproject"></toolbar>
+  </div>
+
 </template>
 
 <script>
 import mapHelper from "./js/mapHelper.js";
 import cesiumloader from "./js/cesiumloader";
-import {getProject,getDefaultcfg}  from "./api/map"
+import { getProject, getDefaultcfg } from "./api/map"
 import toolbar from "./toolbar2.vue"
 
 var viewer = null;
 export default {
-  components: { toolbar},
+  components: { toolbar },
   methods: {
     //根据system信息初始化中间的地图组件，threejs初始化和ceiusm初始化
     initprojectx(project, viewer) {
       if (project) {
         //解析同一个项目使用不同的引擎，这里的userinfo在特定情况下会被使用
-          cesiumloader.loadproject(
-            project,
-            {username:"test",rolename:"test"},
-            getDefaultcfg(),
-            viewer)
+        cesiumloader.loadproject(
+          project,
+          { username: "test", rolename: "test" },
+          getDefaultcfg(),
+          viewer)
       }
     },
     //额外的初始化工作
     init() {
-        viewer = mapHelper.initviewer("easyearthContainer");
-        window.viewer = viewer;
-        window.mapHelper = mapHelper;
+      viewer = mapHelper.initviewer("easyearthContainer");
+      window.viewer = viewer;
+      window.mapHelper = mapHelper;
     },
   },
   beforeDestroy() {
-    if(viewer)
-    {
-        viewer.destroy();
-        viewer=undefined;
+    if (viewer) {
+      viewer.destroy();
+      viewer = undefined;
     }
   },
-  created() {},
+  created() { },
   mounted() {
     this.init();
     //加载项目，加载的内容暂时记录在project中，详细的project描述请参考文档
-    this.curproject= getProject();
-    this.initprojectx(this.curproject,viewer);
-    
+    this.curproject = getProject();
+    this.initprojectx(this.curproject, viewer);
+
     //获取三维球的全局配置，写入到全局中用于相关调用，然后再初始化地球
+
+
+    var layer = new Cesium.MapboxStyleImageryProvider({
+      styleId: 'dark-v10',
+      accessToken: 'pk.eyJ1IjoicG9uZ3hpZSIsImEiOiJjbDVrdmtndHowZHY3M2pxcjdrZzZsZTVoIn0.3PpdH5rSURGGtrfp90iIOw',
+    });
+    viewer.imageryLayers.addImageryProvider(layer);
+
   },
   data() {
     return {
-      showtoolbar:true,
+      showtoolbar: true,
       currentUser: {},
       enableMultiView: false,
       mapSelect: { cursor: "" },
